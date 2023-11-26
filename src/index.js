923631bfb9d64ac98fb2a7c1b4883af35470ba0a
AdrifeCova <adrife2000@hotmail.com>
2023-11-25 19:01:00 -0600

    changes

---
+++ src/index.js
@@ -0,0 +1,17 @@
+let section = document.querySelector('h1');
+let space = document.querySelector('body');
+
+
+
+
+console.log(section);
+
+section.addEventListener('click', () => {
+    section.style.color = 'red'
+})
+
+space.addEventListener('click', () => {
+        space.style.backgroundColor = 'black'
+})
+
+
